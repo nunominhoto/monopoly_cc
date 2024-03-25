68577bbcbe50eaeadab7ac63a7817a18645a475c
NUNOPC\nuno- <nuno-minhoto@hotmail.com>
2024-03-25 18:53:06 +0000

    feat(setup): Built in LED Blink project setup

--- Monopoly_CC/src/main.cpp
+++ Monopoly_CC/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+bool state;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  state = false;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_BUILTIN, state);
+  state = !state;
+  delay(2000); 
 }
