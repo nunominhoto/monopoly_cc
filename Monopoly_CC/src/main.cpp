83ebfed67f5b04edca172fa6e8cc5ce665f4ed94
NUNOPC\nuno- <nuno-minhoto@hotmail.com>
2024-04-10 18:51:01 +0100

    feat(LCD): LCD lib included

--- Monopoly_CC/src/main.cpp
+++ Monopoly_CC/src/main.cpp
@@ -1,16 +1,42 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 bool state;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  
+
+  Wire.begin();
+
+  Serial.begin(9600);
+
+  while (!Serial);
+  Serial.println("\nI2c Scanner");
+
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
+
   state = false;
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+
   digitalWrite(LED_BUILTIN, state);
   state = !state;
-  delay(2000); 
+  delay(300); 
 }
